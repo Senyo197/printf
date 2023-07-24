21228e441adeeac699bce475acc3c598bb11828e
Senyo197 <Senyo197@gmail.com>
2023-07-24 13:18:59 -0700

    format specifier c, s and %

---
+++ printf_cs.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  *_printf - prints format specificier of c, s and %
+  *@format: a character string
+  */
+int _printf(const char *format, ...)
+{
+	int z;
+	char c;
+	char *s
+
+	va_list agmts;
+
+	va_start(agmts, format);
+
+	for (z = 0; z != '\0'; z++)
+	{
+		if (format[z] == '%')
+		{
+			switch (++format)
+			{
+				case 'c':
+					c = va_arg(agmts, int);
+					_putchar(c);
+					break;
+				case 's':
+					s = va_arg(agmts, char *);
+					printf("%s", s);
+					break;
+				case '%':
+					_putchar('%');
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[z]);
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[z]);
+		}
+
+	}
+
+	va_end(agmts);
+	return (0);
+}
