ad5cba55f07f899d43e9724640e076db9e22bbf6
Senyo197 <Senyo197@gmail.com>
2023-07-26 14:54:29 -0700

    correction in the printf_cs.c file

--- printf_cs.c
+++ printf_cs.c
@@ -1,65 +1,61 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
-  *prnt_buf - print buffer content
-  *@buff: array of characters
-  *@buff_indx: buffer index
+  *_printf - A printf function
+  *@format: format specifier
+  *Return: printed character
   */
-void prnt_buf(char buff[], int *buff_indx)
-{
-	if (*buff_indx > 0)
-	{
-		write(1, &buff[0], *buff_indx);
-
-		*buff_indx = 0;
-	}
-}
-
-/**
-  *_printf - prints format specificier of c, s and %
-  *@format: a character string
- *Return: 0
- */
 int _printf(const char *format, ...)
 {
-	int z, p1 = 0, p2 = 0;
-	int width, size, precision, flags, buff_indx = 0;
-	va_list agmts;
-	char buff[BUF_SIZE];
+	int a, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUF_SIZE];
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
+	va_start(list, format);
 
-	va_start(agmts, format);
-	for (z = 0; format && format[z] != '\0'; z++)
+	for (a = 0; format && format[a] != '\0'; a++)
 	{
-		if (format[z] == '%')
+		if (format[a] != '%')
 		{
-			buff[buff_indx++] = format[z];
-
-			if (buff_indx == BUF_SIZE)
-			{
-				prnt_buf(buff, &buff_indx);
-			}
-			p2++;
+			buffer[buff_ind++] = format[a];
+			if (buff_ind == BUF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
 		}
 		else
 		{
-			prnt_buf(buff, &buff_indx);
-			flags = get_flags(format, &z);
-			width = get_width(format, &z, agmts);
-			size = get_size(format, &z);
-			precision = get_precision(format, &z, agmts);
-			z++;
-			p1 = handle_print(format, &z, agmts, buff, size, flags, width,
-					precision);
-			if (p1 == -1)
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &a);
+			width = get_width(format, &a, list);
+			precision = get_precision(format, &a, list);
+			size = get_size(format, &a);
+			++a;
+			printed = handle_print(format, &a, list, buffer, flags, width,
+					precision, size);
+			if (printed == -1)
 				return (-1);
-			p2 += p1;
+			printed_chars += printed;
 		}
 	}
-	va_end(agmts);
-	return (p2);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
+}
+
+/**
+  *print_buffer - Prints the contents of the buffer if it exist
+  *@buffer: Array of chars
+  *@buff_ind: Index at which to add next char, represents the length
+  */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
